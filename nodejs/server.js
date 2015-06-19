2151ca80e02fb7b843772f176a645ca699ef57cf
Thiago <thiago@drc.com>
2015-06-18 22:57:36 -0300

    Starting node

---
+++ nodejs/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var server=http.createServer(function (request, response) {
+    response.writeHead(200);
+    response.write('<h1>Yeah Cience, Bitch!!!!!</h1>');
+    response.end();
+});
+
+server.listen(8080);
+console.log('Server on URL localhost and PORT 8080')
