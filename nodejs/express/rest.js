1caf70a24460e4e40ce83b20b1f6f79aebad51e2
Thiago <thiago@drc.com>
2015-06-19 22:01:09 -0300

    Node REST: Put, Get, Post e Delete

---
+++ nodejs/express/rest.js
@@ -0,0 +1,37 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var getData = function (req) {
+    return{name: req.body.name, method: req.method}
+};
+
+var app = express();
+    app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+
+    app.get('/submit', function(req, res){
+        res.send('<h1>GET ' + req.query.name+'</h1>'
+        +'<h1>Password: ' + req.query.password + '</h1>');
+    });
+
+    app.get('/service/:name', function (req,res) {
+        res.send('<h1>GET '+ req.params.name + '</h1>');
+
+    });
+
+    app.post('/service', function(req, res){
+        res.send(getData(req));
+    });
+
+    app.put('/service', function(req,res){
+        res.send(getData(req));
+    });
+
+    app.delete('/service', function(req,res){
+        res.send(getData(req));
+    });
+
+    app.listen(8080);
+
+console.log('Rest up and running at 8080')
--- nodejs/express/route.js
+++ nodejs/express/route.js
@@ -1,8 +1,18 @@
 var express = require('express'),
     app = express();
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
    res.send('<h1>Hello from NodeJS</h1>');
+});*/
+
+app.use(express.static('public'));
+
+app.get('/service', function (req, res) {
+    res.json({data: 'Some data'});
+});
+
+app.get('/service/:name/:last_name', function (req, res) {
+    res.send('<h1>' +req.params.name +req.params.last_name + ' pra quem voce tira o chapéu?</h1>');
 });
 
 app.listen(8080);
